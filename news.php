04b005014f49b01bc7a6bd617f9235e64726e151
frenz <giuseppe@gtgames.it>
2014-05-24 03:12:04 +0200

    fix get problem. Entity object is different to skillet object. PD

--- news.php
+++ news.php
@@ -1,20 +1,32 @@
 <?php
 
+session_start();
 require_once "include/beContent.inc.php";
 require_once "include/view/template/InitGraphic.php";
 
-$newsPageEntity = $GLOBALS['sys_news'];
+
+$newsEntity = $GLOBALS['sys_news'];
 
 $main = new Skin("theme");
 InitGraphic::getInstance()->createGraphic($main);
 
-//Creazione oggetto di tipo news
-$newsPageTemplate = new Skinlet('newsPage');
-$newsPage = new Content($newsPageEntity);
-$newsPage->setOrderFields("id DESC");
-$newsPage->apply($newsPageTemplate);
 
-$main->setContent("body", $newsPage->get());
-$main->close();  
+
+//Creazione oggetto di tipo news
+$newsTemplate = new Skinlet('newsPage');
+$news = new Content($newsEntity);
+$news->setOrderFields("id DESC");
+$news->apply($newsTemplate);
+
+
+
+
+$home = new Skinlet("home");
+
+
+$main->setContent("news", $newsTemplate->get());
+
+
+$main->close();
 
 ?>
